c09006d4747cea1d6b1b80b2da1f4eb4d60cd9cb
MendoBoss <admiralmendo@hotmail.com>
2024-06-27 10:12:11 -0400

    Model/migrate/factory

---
+++ database/factories/BienFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Bien>
+ */
+class BienFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'category_id' => fake()->randomElement([1,2,3,4,5]),
+            'user_id' => fake()->randomElement([1,2,3,4,5,6,7,8,9,10]),
+            'state' => fake()->randomElement(['libre','occupé']),
+            'title' => fake()->word(),
+            'description' => fake()->sentence(),
+            'address' => fake()->address(),
+            'ville' => fake()->city(),
+            'price' => fake()->randomFloat(2,500,1500),
+            'caution' => fake()->randomFloat(2,500,800),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Bien;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\State;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
+        Category::factory(5)->create();
+        Bien::factory(100)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
