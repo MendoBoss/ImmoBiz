c5996129ad79063893dcfaa1512aac0584c68010
MendoBoss <admiralmendo@hotmail.com>
2024-06-27 10:26:53 -0400

    update

--- app/Models/Bien.php
+++ app/Models/Bien.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Bien extends Model
 {
     use HasFactory;
+    public function user(){
+        return $this->belognsTo(User::class);
+    }
+    public function category(){
+        return $this->belognsTo(Category::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Bien;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    public function biens(){
+        return $this->hasMany(Bien::class);
+    }
 
     /**
      * The attributes that are mass assignable.
